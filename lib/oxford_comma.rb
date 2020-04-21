d5b3d30a363288e9f7cc77032499226aaeacf298
Omar Montemayor <omarmontemayor11@yahoo.com>
2020-04-21 14:59:27 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,10 @@
 def oxford_comma(array)
   i = array.size
-  when i == 1
-    
-      
-    
+  if i == 1
+    array.join
+  elsif i == 2
+    array.join(" and ")
+  else
+    array[0..-2].join(", ") + ", and " + array[-1]
+  end
 end
