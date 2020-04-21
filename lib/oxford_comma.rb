a25b6c9abe85905264f51c279cd4611956ea1064
Omar Montemayor <omarmontemayor11@yahoo.com>
2020-04-21 14:54:58 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,7 @@
 def oxford_comma(array)
   i = array.size
-  if i == 1 
-    array.join
-  elsif i == 2 
-    array.join(" and ")
-  elsif i == 3
-    if i < 3
-      array.join(", ")
+  when i == 1
+    
+      
     
 end
